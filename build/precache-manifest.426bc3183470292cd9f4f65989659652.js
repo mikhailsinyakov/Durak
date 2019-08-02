df368aedffeaba9256f067cb74000276ae229e83
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2019-08-02 18:11:26 +0300

    Add build folder

---
+++ build/precache-manifest.426bc3183470292cd9f4f65989659652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6465dcbd8eb63ac57d8a992dfabd98bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2117058c5baabc8691a",
+    "url": "/static/css/main.dce03e38.chunk.css"
+  },
+  {
+    "revision": "7097eddf229b6e5dd8fa",
+    "url": "/static/js/2.aeaa003f.chunk.js"
+  },
+  {
+    "revision": "d2117058c5baabc8691a",
+    "url": "/static/js/main.87a46d40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
